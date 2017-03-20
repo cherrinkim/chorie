79ea0371f34a7be2ed1401117315ac65ff64bd0d
Cherrin Kim <cherrynkinnz@gmail.com>
2017-03-20 15:21:12 -0400

    set up of dropdown page

---
+++ chorie/OpenScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace chorie
+{
+	public class OpenScreen : ContentPage
+	{
+		public OpenScreen()
+		{
+			Label header = new Label
+			{
+				Text = "Choose template",
+				FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+				HorizontalOptions = LayoutOptions.Center
+			};
+
+			Picker picker = new Picker
+			{
+				Title = "Formation",
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
+
+			picker.Items.Add("Horizontal Line");
+			picker.Items.Add("Vertical Line");
+			picker.Items.Add("Circle");
+
+			Content = new StackLayout
+			{
+				Children =
+				{
+					header,
+					picker,
+				}
+			};
+
+		}
+	}
+}
+
