d0e6aff8f07b0ed6ce79a60623ef9b3e7abf91ba
Cherrin Kim <cherrynkinnz@gmail.com>
2017-03-28 11:53:28 -0400

    stack layout within absolute layout

--- chorie/ActvityPage.cs
+++ chorie/ActvityPage.cs
@@ -8,41 +8,24 @@ namespace chorie
 	{
 		public ActvityPage()
 		{
-			Title = "Absolute Layout Exploration - Code";
 			var layout = new AbsoluteLayout();
 
-			var centerLabel = new Label
-			{
-				Text = "I'm centered on iPhone 4 but no other device.",
-				LineBreakMode = LineBreakMode.WordWrap
-			};
+			var bottomStack = new StackLayout {BackgroundColor = Color.Olive };
+			AbsoluteLayout.SetLayoutBounds(bottomStack, new Rectangle(0, 1, 1, .15));
+			AbsoluteLayout.SetLayoutFlags(bottomStack, AbsoluteLayoutFlags.All);
 
-			AbsoluteLayout.SetLayoutBounds(centerLabel, new Rectangle(115, 159, 100, 100));
-			// No need to set layout flags, absolute positioning is the default
+			var leftStack = new StackLayout { BackgroundColor = Color.Red };
+			AbsoluteLayout.SetLayoutBounds(leftStack, new Rectangle(0, 0, .15, .85));
+			AbsoluteLayout.SetLayoutFlags(leftStack, AbsoluteLayoutFlags.All);
 
-			var bottomLabel = new Label { Text = "I'm bottom center on every device.", LineBreakMode = LineBreakMode.WordWrap };
-			AbsoluteLayout.SetLayoutBounds(bottomLabel, new Rectangle(.5, 1, .5, .1));
-			AbsoluteLayout.SetLayoutFlags(bottomLabel, AbsoluteLayoutFlags.All);
-
-			var rightBox = new BoxView { Color = Color.Olive };
-			AbsoluteLayout.SetLayoutBounds(rightBox, new Rectangle(1, .5, 25, 100));
-			AbsoluteLayout.SetLayoutFlags(rightBox, AbsoluteLayoutFlags.PositionProportional);
-
-			var leftBox = new BoxView { Color = Color.Red };
-			AbsoluteLayout.SetLayoutBounds(leftBox, new Rectangle(0, 0, 200, AbsoluteLayout.AutoSize));
-			AbsoluteLayout.SetLayoutFlags(leftBox, AbsoluteLayoutFlags.PositionProportional);
-
-			var topBox = new BoxView { Color = Color.Blue };
-			AbsoluteLayout.SetLayoutBounds(topBox, new Rectangle(.5, 0, 100, 25));
-			AbsoluteLayout.SetLayoutFlags(topBox, AbsoluteLayoutFlags.PositionProportional);
-
-			layout.Children.Add(bottomLabel);
-			layout.Children.Add(centerLabel);
-			layout.Children.Add(rightBox);
-			layout.Children.Add(leftBox);
-			layout.Children.Add(topBox);
+			layout.Children.Add(bottomStack);
+			layout.Children.Add(leftStack);
 
 			Content = layout;
+
+
+
+
 		}
 	}
 }
